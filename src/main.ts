36b5e37fdb1cbd469e65bd122bd4adf32051bcb9
Filipsys <filipsysak@gmail.com>
2025-03-06 01:26:07 +0100

    Slight paths optimisation, performance checks for draw and update & code cleanup

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ if (!canvas) throw new Error("Canvas element not found.");
 const context = canvas.getContext("2d");
 if (!context) throw new Error("Context not found.");
 
-const FRAMERATE = 5;
+const FRAMERATE = 10;
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 const canvasColor = "#fff";
@@ -22,21 +22,14 @@ let previousTimestamp = 0;
 let timeElapsed = 0;
 let canvasElements = new Set<Element>();
 let pathConnections = new Set<{ x1: number; y1: number; x2: number; y2: number }>();
-
-const drawPath = (x1: number, y1: number, x2: number, y2: number) => {
-  context.strokeStyle = pathColor;
-
-  context.beginPath();
-  context.moveTo(x1, y1);
-  context.lineTo(x2, y2);
-  context.closePath();
-  context.stroke();
-};
+let elementsChanged = false;
 
 const draw = () => {
+  const startTime = performance.now();
+
+  // Clear the canvas
   context.fillStyle = canvasColor;
   context.fillRect(0, 0, canvasWidth, canvasHeight);
-  console.log("Cleared canvas");
 
   // Draw the elements
   context.fillStyle = elementColor;
@@ -47,20 +40,39 @@ const draw = () => {
     context.fill();
   });
 
-  // pathConnections.forEach((path) => drawPath);
+  // Draw the paths
+  pathConnections.forEach((path) => {
+    context.strokeStyle = pathColor;
+
+    context.beginPath();
+    context.moveTo(path.x1, path.y1);
+    context.lineTo(path.x2, path.y2);
+    context.closePath();
+    context.stroke();
+  });
+
+  console.debug("Draw function took ", (performance.now() - startTime).toFixed(2), "ms");
 };
 
 const update = () => {
+  if (!elementsChanged) return;
+  const startTime = performance.now();
+
   canvasElements.forEach((element) => {
     canvasElements.forEach((loopedElement) => {
       if (element === loopedElement) return;
       if (!element.tags || !loopedElement.tags) return;
+      if (!element.tags.intersection(loopedElement.tags)) return;
 
-      if (element.tags.intersection(loopedElement.tags)) {
-        pathConnections.add({ x1: element.x, y1: element.y, x2: loopedElement.x, y2: loopedElement.y });
-      }
+      // I'm not sure why this code generates more paths than necessary,
+      // I think it may be because it's adding paths that are reversed.
+      pathConnections.add({ x1: element.x, y1: element.y, x2: loopedElement.x, y2: loopedElement.y });
+      console.log(pathConnections);
     });
   });
+
+  elementsChanged = false;
+  console.debug("Update function took ", (performance.now() - startTime).toFixed(2), "ms");
 };
 
 const animationLoop = (timestamp: number) => {
@@ -80,5 +92,7 @@ const animationLoop = (timestamp: number) => {
 window.requestAnimationFrame(animationLoop);
 
 canvas.addEventListener("click", (event) => {
-  canvasElements.add({ x: event.clientX, y: event.clientY, radius: 5, tags: new Set(["tag1"]) });
+  canvasElements.add({ x: event.clientX, y: event.clientY, radius: 5, tags: new Set(["tag1", "tag2"]) });
+
+  elementsChanged = true;
 });
